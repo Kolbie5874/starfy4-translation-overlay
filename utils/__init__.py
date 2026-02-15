e23223eef31f43925189f318a41393dca342bdd8
Ari <basicallyahri@proton.me>
2026-02-15 01:31:30 -0600

    utils: expose VLC systemcheck function

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,3 +1,15 @@
 """
 Starfy4 Translation Overlay Utils
 """
+
+from . import systemcheck
+
+def check_for_vlc() -> bool:
+    """"
+    Checks if VLC is installed.
+
+    Returns:
+        bool: True if VLC is installed, False otherwise.
+    """
+
+    return systemcheck.vlc_installed()
